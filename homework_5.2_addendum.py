ba582fc7d2ae3218ad1904ff436c55440e26a033
myshasolin <myshasolin@yandex.ru>
2021-11-26 22:33:21 +0300

    Дополнительный
    с подсчётом не символов в строке, а просто слов

--- homework_5.2_addendum.py
+++ homework_5.2_addendum.py
@@ -4,10 +4,10 @@
 with open('file_dz_5.2.txt', encoding='utf-8') as f:
     print(f'вот что в нашем файле записано:\n\n{f.read()}')
     f.seek(0)
-    reeds = f.readlines(-1)
-    reeds[-1] += ' '
+    reeds = f.readlines()
+
     print(f'\nИтого\nколичество строк: {len(reeds)}')
     str_num = 1
     for i in reeds:
-        print(f'в {str_num} строке символов: {len(i)-1} шт.')
+        print(f'в {str_num} строке слов: {len(i.split())} шт.')
         str_num += 1
